4e32c6d88381248c7a1cdf29d64876f9235eb4dd
Craig <cav9871@saintjoe.edu>
2015-02-04 11:36:09 -0500

    This is my ch.4 hw for javascriptClass

---
+++ reverseArray.js
@@ -0,0 +1,27 @@
+// Craig Austgen
+// reverse array for javascript class
+//
+
+
+
+function reverseArray(array) {
+	  var output = [];                                 
+	    for (var i = array.length - 1; i >= 0; i--)
+		        output.push(array[i]);
+	      return output;
+}
+
+function reverseArrayInPlace(array) {
+	  for (var i = 0; i < Math.floor(array.length / 2); i++) {
+		      var old = array[i];
+		          array[i] = array[array.length - 1 - i];
+			      array[array.length - 1 - i] = old;
+			        }
+	    return array;
+}
+
+
+console.log(reverseArray(['X' 'Y', 'Z']));         // reverses the order of this array
+ var arrayVal = [1, 2, 3, 4, 5];                   // declares a new array
+ reverseArrayInPlace(arrayValue);                  
+ console.log(arrayValue);
